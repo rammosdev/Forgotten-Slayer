451f1a7409a6025242ac4404d6a886ffbbb9c871
predo2810 <pedromontanari09@gmail.com>
2022-08-20 20:15:25 -0300

    Atualizações
    Mudanças na movimentação da câmera;
    Adicionado a movimentação da espada;
    Alguns comentários nos scripts;

--- Forgotten Slayer/Assets/Scripts/CameraSystem.cs
+++ Forgotten Slayer/Assets/Scripts/CameraSystem.cs
@@ -1,28 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSystem : MonoBehaviour
 {
-    private GameObject Player;
+    private Transform Player;
 
-    public float minX, minY, speed;
+    public float Speed;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
+        // Pega os componentes necessários
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    private void FixedUpdate() {
-        transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+    private void FixedUpdate()
+    {
+        // Movimenta a camera
+        transform.position = new Vector2(Mathf.Lerp(transform.position.x, Player.position.x, Speed * Time.deltaTime), Mathf.Lerp(transform.position.y, Player.position.y, Speed * Time.deltaTime));
 
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, 
+        // Vou deixar aqui pra se você ainda quiser usar :)
+        // Pooorém, o código de cima eu acho um pouco melhor e mais "macio"
+        /*transform.position = new Vector2(Mathf.Clamp(transform.position.x, 
         Player.GetComponent<Transform>().position.x - minX, 
         Player.GetComponent<Transform>().position.x + minX),
 
         Mathf.Clamp(transform.position.y, 
         Player.GetComponent<Transform>().position.y - minY, 
-        Player.GetComponent<Transform>().position.y + minY));
+        Player.GetComponent<Transform>().position.y + minY));*/
     }
 }
--- Forgotten Slayer/Assets/Scripts/PlayerController.cs
+++ Forgotten Slayer/Assets/Scripts/PlayerController.cs
@@ -1,55 +1,82 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject HandDistance;
+    public GameObject Axis;
     public ParticleSystem dust;
     public Rigidbody2D rig;
     public Animator anima;
     public Vector2 playerInput;
     public float moveSpeed;
 
-    private void Awake() {
+    public bool Flip;
+
+    private void Awake()
+    {
+        // Pega os componentes necessários.
         rig = GetComponent<Rigidbody2D>();
         anima = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void MouseLook()
     {
+        // Calcula a rotação da espada do jogador baseado na posição do mouse
+        Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 LookDir = MousePos - Axis.transform.position;
+        float Angle = Mathf.Atan2(LookDir.y, LookDir.x) * Mathf.Rad2Deg - 90f;
+        Axis.GetComponent<Rigidbody2D>().rotation = Angle;
         
+        // Vira alguns objetos baseado na rotação
+        if (LookDir.x > 0)
+        {
+            Flip = false;
+            HandDistance.transform.localRotation = Quaternion.Euler(0, HandDistance.transform.rotation.y, HandDistance.transform.rotation.z);
+        }
+        else
+        {
+            Flip = true;
+            HandDistance.transform.localRotation = Quaternion.Euler(180, HandDistance.transform.rotation.y, HandDistance.transform.rotation.z);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
-    }
-    private void FixedUpdate() {
+        // Chama as funções depois do cálculo das físicas do jogo.
+        MouseLook();
         Movimento();
         Animation();
+
+        // Vira o jogador horizontalmente baseado em certas circunstâncias
+        GetComponent<SpriteRenderer>().flipX = Flip;
     }
 
+    // Movimento
     void Movimento()
     {
+        // Define a direção de movimento baseado nas teclas pressionadas.
         playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        // Adiciona velocidade ao jogador baseado na direção.
         rig.velocity = playerInput.normalized * moveSpeed;
 
+        // Muda a direção do player baseado em sua velocidade
         if (playerInput.x > 0 || playerInput.y > 0)
         {
-            Debug.Log("Andando");
-            transform.eulerAngles = new Vector3(0,0,0);
+            Debug.Log("Andando Direita");
+            Flip = false;
             CreateDust();
         }
         
         if (playerInput.x < 0)
         {
-            Debug.Log("Andando");
-            transform.eulerAngles = new Vector3(0,180,0);
+            Debug.Log("Andando Esquerda");
+            
+            Flip = true;
             CreateDust();
         }
 
+        // Parado. Só isso.
         else if (playerInput.x == 0 || playerInput.y == 0 )
         {
             Debug.Log("Parado");
@@ -57,6 +84,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    // Animação
     void Animation()
     {
         if (playerInput.x > 0 || playerInput.y > 0)
